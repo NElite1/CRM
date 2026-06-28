20f98f2f8a8fbadf1327932eaea314d7d33ab48a
NElite1 <parvizan24@gmail.com>
2026-06-28 09:43:42 +0500

    V0.1.1   home page is developing

---
+++ CRM.js
@@ -0,0 +1,16 @@
+const cards = document.querySelectorAll('.info-card');
+const detailCopy = document.querySelector('.detail-copy');
+
+function setActiveCard(card) {
+  cards.forEach((item) => item.classList.remove('active'));
+  card.classList.add('active');
+  detailCopy.textContent = card.dataset.detail;
+}
+
+cards.forEach((card) => {
+  card.addEventListener('click', () => setActiveCard(card));
+});
+
+if (cards.length > 0) {
+  setActiveCard(cards[0]);
+}
